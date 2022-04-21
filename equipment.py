be680fa742111dc209263ff4bd94391c56791c87
jongmindev <jongmin.dev@gmail.com>
2022-04-22 05:23:46 +0900

    2022-04-22 Modify the structure of classes. ... Change name of classes. Respond special cases such as no potential / no scroll.

---
+++ equipment.py
@@ -0,0 +1,314 @@
+from parsetag import BrowserForEquipmentTag, ParseInfoTag
+from bs4.element import Tag
+import re
+
+
+class EquipmentInformation:
+    """
+    name, scroll, category, stats_dict, potential, additional, starforce, superior, (amazing,) hammer
+    """
+    def __init__(self, parsed_tag: ParseInfoTag):
+        self._parsed_tag = parsed_tag
+        self._name = self._set_title()
+        self._scroll = self._set_scroll()
+        self._category = self._set_category()
+        self._stats_dict = self._set_stats_dict()
+        self._potential = self._set_potential()
+        self._additional = self._set_additional_potential()
+        starforce_tuple = self._set_starforce()
+        self._starforce_max = starforce_tuple[0]
+        self._starforce_now = starforce_tuple[1]
+        self._superior = self._set_superior()
+        self._amazing = self._set_amazing()
+        self._hammer = self._set_hammer()
+
+    def _set_title(self) -> str:
+        """parsed_tag.title -> name of the equip"""
+        if self._parsed_tag.title[-1] != ")":
+            equip_name = self._parsed_tag.title
+        else:
+            pattern = re.compile(r".+(?=\s\(\+\d+\))")
+            equip_name = pattern.search(self._parsed_tag.title).group()
+        self._type_checker(equip_name, str)
+        return equip_name
+
+    def _set_scroll(self) -> int:
+        """parsed_tag.title -> scroll success times"""
+        if self._parsed_tag.title[-1] != ")":
+            scroll_times = 0
+        else:
+            pattern = re.compile(r"\s\(\+(\d+)\)")
+            scroll_times = pattern.search(self._parsed_tag.title).group(1)
+        scroll_times = int(scroll_times)
+        self._type_checker(scroll_times, int)
+        return scroll_times
+
+    def _set_category(self) -> str:
+        """parsed_tag.category -> category of the equip"""
+        self._type_checker(self._parsed_tag.category, str)
+        equip_category = self._parsed_tag.category
+        return equip_category
+
+    def _set_stats_dict(self) -> dict:
+        """parsed_tag.stats_dict -> dict of STR/DEX/INT/LUK/MaxHP/Defense/AllStats/
+        AttackPower/MagicAttack/BossDamage/IgnoreDEF/... -> """
+        equip_stats_dict = {}
+        for key_str, value_tag in self._parsed_tag.stats_dict.items():
+            if (key_str[:4] == "잠재옵션") | (key_str[:9] == "에디셔널 잠재옵션") | (key_str == "기타"):
+                pass
+            else:
+                equip_stats_dict[key_str] = value_tag.text
+        return equip_stats_dict
+
+    def _set_potential(self) -> tuple[str, list]:
+        """parsed_tag.stats_dict -> tier and 3 options of potential"""
+        tier = '일반'
+        options_list = []
+        for key_str, value_tag in self._parsed_tag.stats_dict.items():
+            if key_str[:4] == "잠재옵션":
+                pattern = re.compile(r"\((\w+).+\)")
+                if pattern.search(key_str) is not None:
+                    tier = pattern.search(key_str).group(1)
+                options_list = value_tag.get_text(strip=True, separator='\n').splitlines()
+            else:
+                pass
+        return tier, options_list
+
+    def _set_additional_potential(self) -> tuple[str, list]:
+        """parsed_tag.stats_dict -> tier and 3 options of additional potential"""
+        tier = '일반'
+        options_list = []
+        for key_str, value_tag in self._parsed_tag.stats_dict.items():
+            if key_str[:9] == "에디셔널 잠재옵션":
+                pattern = re.compile(r"\((\w+).+\)")
+                if pattern.search(key_str) is not None:
+                    tier = pattern.search(key_str).group(1)
+                options_list = value_tag.get_text(strip=True, separator='\n').splitlines()
+            else:
+                pass
+        return tier, options_list
+
+    def _etc_to_list(self) -> list[str]:
+        """parsed_tag.stats_dict 의 기타의 값 -> list split"""
+        etc_list = []
+        for key_str, value_tag in self._parsed_tag.stats_dict.items():
+            if key_str == "기타":
+                etc_list = value_tag.get_text(strip=True, separator='\n').splitlines()
+        return etc_list
+
+    def _set_starforce(self) -> tuple[int, int]:
+        """parsed_tag.stats_dict -> (max starforce, starforce of the equip)"""
+        etc_list = self._etc_to_list()
+        star_values = []
+        for text in etc_list:
+            if text.__contains__("성까지 강화 가능"):
+                positive_integers_str = ''.join((ch if ch in '0123456789' else ' ') for ch in text).split()
+                for integer in positive_integers_str:
+                    star_values.append(int(integer))
+            else:
+                pass
+        if len(star_values) == 0:
+            star_values = (0, 0)
+        elif len(star_values) == 1:
+            star_values = (star_values[-1], 0)
+        elif len(star_values) == 2:
+            star_values = (star_values[-1], star_values[0])
+        else:
+            raise ValueError("Wrong starforce parsing.")
+        return star_values
+
+    def _set_superior(self) -> bool:
+        """parsed_tag.stats_dict -> whether to be superior item"""
+        etc_list = self._etc_to_list()
+        superior = False
+        for text in etc_list:
+            if text.__contains__("슈페리얼"):
+                superior = True
+        return superior
+
+    def _set_amazing(self) -> bool:
+        """미구현미구현미구현미구현미구현미구현미구현"""
+        return False
+
+    def _set_hammer(self) -> bool:
+        """parsed_tag.stats_dict -> whether to use hammer"""
+        etc_list = self._etc_to_list()
+        hammer_used = False
+        for text in etc_list:
+            if text.__contains__("황금망치 제련 적용"):
+                hammer_used = True
+        return hammer_used
+
+    @staticmethod
+    def _type_checker(checked, type_restraint: object):
+        if type(checked) != type_restraint:
+            raise TypeError(f"{checked} : Wrong type. Should be {type_restraint}, but it is {type(type_restraint)}.")
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def scroll(self):
+        return self._scroll
+
+    @property
+    def category(self):
+        return self._category
+
+    @property
+    def stats_dict(self):
+        return self._stats_dict
+
+    @property
+    def potential(self):
+        return self._potential
+
+    @property
+    def additional(self):
+        return self._additional
+
+    @property
+    def starforce_max(self):
+        return self._starforce_max
+
+    @property
+    def starforce_now(self):
+        return self._starforce_now
+
+    @property
+    def superior(self):
+        return self._superior
+
+    @property
+    def amazing(self):
+        return self._amazing
+
+    @property
+    def hammer(self):
+        return self._hammer
+
+
+class TrimmedInformation(EquipmentInformation):
+    def __init__(self, equipment_info_tag: Tag):
+        parsed_tag = ParseInfoTag(equipment_info_tag)
+        super(TrimmedInformation, self).__init__(parsed_tag)
+        self._stat_options = self._trim_stats_dict()
+        self._potential_tier = super().potential[0]
+        self._potential_options = self._trim_potential(super().potential)
+        self._additional_tier = super().additional[0]
+        self._additional_options = self._trim_potential(super().additional)
+
+    def _trim_stats_dict(self) -> dict:
+        trimmed = {}
+        for key, value in super().stats_dict.items():
+            trimmed[key] = value.split()[0]
+        return trimmed
+
+    def _trim_potential(self, super_pot_add) -> dict:
+        trimmed = {}
+        for line in super_pot_add[1]:
+            if line.__contains__(' : '):
+                stat, value = line.split(' : ')
+                try:
+                    trimmed[stat] += value
+                except KeyError:
+                    trimmed[stat] = value
+            else:
+                try:
+                    trimmed[line] += 1
+                except KeyError:
+                    trimmed[line] = 1
+        return trimmed
+
+    # def _trim_additional(self) -> dict:
+    #     trimmed = {}
+    #     for line in super().additional[1]:
+    #         if line.__contains__(' : '):
+    #             stat, value = line.split(' : ')
+    #             try:
+    #                 trimmed[stat] += value
+    #             except KeyError:
+    #                 trimmed[stat] = value
+    #         else:
+    #             try:
+    #                 trimmed[line] += 1
+    #             except KeyError:
+    #                 trimmed[line] = 1
+    #     return trimmed
+
+    @property
+    def stat_options(self):
+        return self._stat_options
+
+    @property
+    def potential_tier(self):
+        return self._potential_tier
+
+    @property
+    def potential_options(self):
+        return self._potential_options
+
+    @property
+    def additional_tier(self):
+        return self._additional_tier
+
+    @property
+    def additional_options(self):
+        return self._additional_options
+
+
+# class MyEquipment:
+#     def __init__(self):
+#         pass
+#
+#     def _whether_to_equip(self) -> bool:
+#         pass
+
+
+if __name__ == "__main__":
+    # import geturl
+    # my_url = geturl.GetDetailEquipmentUrl("히슈와").equipment_url
+    # open_browser = BrowserForEquipmentTag(url=my_url)
+    # item_tag = open_browser.get_equipment_info_tag(22)
+    # open_browser.quit_browser()
+    # item_parsed_tag = ParseInfoTag(item_tag)
+    # item_second_step = EquipmentInformation(item_parsed_tag)
+    # # item_information = equipment.EquipmentInformation
+    #
+    # print("name__", item_second_step.name)
+    # print("scroll", item_second_step.scroll)
+    # print("cate__", item_second_step.category)
+    # print("stat__", item_second_step.stats_dict)
+    # print("poten_", item_second_step.potential)
+    # print("addi__", item_second_step.additional)
+    # print("star_m", item_second_step.starforce_max)
+    # print("star_n", item_second_step.starforce_now)
+    # print("superi", item_second_step.superior)
+    # print("amazin", item_second_step.amazing)
+    # print("hammer", item_second_step.hammer)
+
+    import geturl
+    my_url = geturl.GetDetailEquipmentUrl("히슈와").equipment_url
+    open_browser = BrowserForEquipmentTag(url=my_url)
+    item_info_tag = open_browser.get_equipment_info_tag('반지1')
+    open_browser.quit_browser()
+
+    information = TrimmedInformation(item_info_tag)
+
+    print("name__", information.name)
+    print("scroll", information.scroll)
+    print("cate__", information.category)
+    # print("stat__", information.stats_dict)
+    print("t_stat", information.stat_options)
+    # print("poten_", information.potential)
+    print("potier", information.potential_tier)
+    print("pot_op", information.potential_options)
+    # print("addi__", information.additional)
+    print("adtier", information.additional_tier)
+    print("add_op", information.additional_options)
+    print("star_m", information.starforce_max)
+    print("star_n", information.starforce_now)
+    print("superi", information.superior)
+    print("amazin", information.amazing)
+    print("hammer", information.hammer)
--- parsetag.py
+++ parsetag.py
@@ -2,31 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-import re
 
 
-class EquipInfoTag:
+EQUIPMENT_INDEX = {'반지1': 1, '모자': 3, '엠블렘': 5,
+                   '반지2': 6, '펜던트1': 7, '얼굴장식': 8, '뱃지': 10,
+                   '반지3': 11, '펜던트2': 12, '눈장식': 13, '귀고리': 14, '훈장': 15,
+                   '반지4': 16, '무기': 17, '상의': 18, '어깨장식': 19, '보조무기': 20,
+                   '포켓': 21, '벨트': 22, '하의': 23, '장갑': 24, '망토': 25,
+                   '신발': 28, '암드로이드': 29, '기계심장': 30}
+
+
+class BrowserForEquipmentTag:
     def __init__(self, url: str):
         # 캐릭터 정보창 url 을 입력받아 브라우저 실행
         self._browser = webdriver.Chrome()
         self._browser.get(url)
 
-    def _get_equip_element(self, item: str | int):
-        """장비 부위 이름 str 또는 li:nth-child int 를 받아서 해당 장비 WebElement 반환"""
+    def _get_equipment_webelement(self, item: str | int):
+        """장비 부위 이름 str 또는 li:(n)th-child int n을 받아서 해당 장비 WebElement 반환"""
         if type(item) == int:
             element = self._browser.find_element(by=By.CSS_SELECTOR,
                                                  value=f"#container ul.item_pot > li:nth-child({item})")
         elif type(item) == str:
-            # equip = dict[equip]
+            item = EQUIPMENT_INDEX[item]
             element = self._browser.find_element(by=By.CSS_SELECTOR,
                                                  value=f"#container ul.item_pot > li:nth-child({item})")
         else:
             raise TypeError("parameter : str or int")
         return element
 
-    def get_equip_info_tag(self, equip: str | int):
+    def get_equipment_info_tag(self, equip: str | int):
         # 찾고자 하는 장비를 클릭
-        item_element = self._get_equip_element(equip)
+        item_element = self._get_equipment_webelement(equip)
         item_element.click()
         # 클릭 이후 바뀐 html get
         html = self._browser.page_source
@@ -36,23 +43,25 @@ class EquipInfoTag:
         item_info = soup.select_one("#container div.tab01_con_wrap > div.item_info > div")
         return item_info
 
-    def browser_quit(self):
+    def quit_browser(self):
         self._browser.quit()
 
 
-class FirstParsingInfoTag:
-    def __init__(self, equip_info_tag: Tag):
-        self._equip_tag = equip_info_tag
+class ParseInfoTag:
+    def __init__(self, equipment_info_tag: Tag):
+        self._equip_tag = equipment_info_tag
         self._title = self._equip_title()
         self._category = self._equip_category()
         self._stats_dict = self._equip_stats_tag_dict()
 
     def _equip_title(self):
         title_tag = self._equip_tag.select_one(".item_title h1")
-        title_tag.select_one("em").extract()
         title_text = title_tag.text
         title_text = title_text.replace("\n", "")
         title_text = title_text.replace(u"\xa0", "")
+        title_text = title_text.strip()
+        if title_text.__contains__("성 강화"):
+            title_text = title_text[:-6]
         title_text = title_text.strip()
         return title_text
 
@@ -85,212 +94,3 @@ class FirstParsingInfoTag:
     @property
     def stats_dict(self):
         return self._stats_dict
-
-
-class SecondParsingInfoTag:
-    """
-    name, scroll, category, stats_dict, potential, additional, starforce, superior, (amazing,) hammer
-    """
-    def __init__(self, first: FirstParsingInfoTag):
-        self._first = first
-        self._name = self._set_title()
-        self._scroll = self._set_scroll()
-        self._category = self._set_category()
-        self._stats_dict = self._set_stats_dict()
-        self._potential = self._set_potential()
-        self._additional = self._set_additional_potential()
-        starforce_tuple = self._set_starforce()
-        self._starforce_max = starforce_tuple[0]
-        self._starforce_now = starforce_tuple[1]
-        self._superior = self._set_superior()
-        self._hammer = self._set_hammer()
-
-    def _set_title(self) -> str:
-        """first.title -> name of the equip"""
-        if self._first.title[-1] != ")":
-            equip_name = self._first.title
-        else:
-            pattern = re.compile(r".+(?=\s\(\+\d+\))")
-            equip_name = pattern.search(self._first.title).group()
-        self._type_checker(equip_name, str)
-        return equip_name
-
-    def _set_scroll(self) -> int:
-        """first.title -> scroll success times"""
-        if self._first.title[-1] != ")":
-            scroll_times = 0
-        else:
-            pattern = re.compile(r"\s\(\+(\d+)\)")
-            scroll_times = pattern.search(lightly_parsed.title).group(1)
-        scroll_times = int(scroll_times)
-        self._type_checker(scroll_times, int)
-        return scroll_times
-
-    def _set_category(self) -> str:
-        """first.category -> category of the equip"""
-        self._type_checker(self._first.category, str)
-        equip_category = self._first.category
-        return equip_category
-
-    def _set_stats_dict(self) -> dict:
-        """first.stats_dict -> dict of STR/DEX/INT/LUK/MaxHP/Defense/AllStats/
-        AttackPower/MagicAttack/BossDamage/IgnoreDEF/... -> """
-        equip_stats_dict = {}
-        for key_str, value_tag in self._first.stats_dict.items():
-            if (key_str[:4] == "잠재옵션") | (key_str[:9] == "에디셔널 잠재옵션") | (key_str == "기타"):
-                pass
-            else:
-                equip_stats_dict[key_str] = value_tag.text
-        return equip_stats_dict
-
-    def _set_potential(self) -> tuple[str, list]:
-        """first.stats_dict -> tier and 3 options of potential"""
-        tier = '일반'
-        options_list = []
-        for key_str, value_tag in self._first.stats_dict.items():
-            if key_str[:4] == "잠재옵션":
-                pattern = re.compile(r"\((\w+).+\)")
-                tier = pattern.search(key_str).group(1)
-                options_list = value_tag.get_text(strip=True, separator='\n').splitlines()
-            else:
-                pass
-        return tier, options_list
-
-    def _set_additional_potential(self) -> tuple[str, list]:
-        """first.stats_dict -> tier and 3 options of additional potential"""
-        tier = '일반'
-        options_list = []
-        for key_str, value_tag in self._first.stats_dict.items():
-            if key_str[:9] == "에디셔널 잠재옵션":
-                pattern = re.compile(r"\((\w+).+\)")
-                tier = pattern.search(key_str).group(1)
-                options_list = value_tag.get_text(strip=True, separator='\n').splitlines()
-            else:
-                pass
-        return tier, options_list
-
-    def _etc_to_list(self) -> list[str]:
-        """first.stats_dict 의 기타의 값 -> list split"""
-        etc_list = []
-        for key_str, value_tag in self._first.stats_dict.items():
-            if key_str == "기타":
-                etc_list = value_tag.get_text(strip=True, separator='\n').splitlines()
-        return etc_list
-
-    def _set_starforce(self) -> tuple[int, int]:
-        """first.stats_dict -> (max starforce, starforce of the equip)"""
-        etc_list = self._etc_to_list()
-        star_values = []
-        for text in etc_list:
-            if text.__contains__("성까지 강화 가능"):
-                positive_integers_str = ''.join((ch if ch in '0123456789' else ' ') for ch in text).split()
-                for integer in positive_integers_str:
-                    star_values.append(int(integer))
-            else:
-                pass
-        if len(star_values) == 0:
-            star_values = (0, 0)
-        elif len(star_values) == 1:
-            star_values = (star_values[-1], 0)
-        elif len(star_values) == 2:
-            star_values = (star_values[-1], star_values[0])
-        else:
-            raise ValueError("Wrong starforce parsing.")
-        return star_values
-
-    def _set_superior(self) -> bool:
-        """first.stats_dict -> whether to be superior item"""
-        etc_list = self._etc_to_list()
-        superior = False
-        for text in etc_list:
-            if text.__contains__("슈페리얼"):
-                superior = True
-        return superior
-
-    def _set_hammer(self) -> bool:
-        """first.stats_dict -> whether to use hammer"""
-        etc_list = self._etc_to_list()
-        hammer_used = False
-        for text in etc_list:
-            if text.__contains__("황금망치 제련 적용"):
-                hammer_used = True
-        return hammer_used
-
-    @staticmethod
-    def _type_checker(checked, type_restraint: object):
-        if type(checked) != type_restraint:
-            raise TypeError(f"{checked} : Wrong type. Should be {type_restraint}, but it is {type(type_restraint)}.")
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def scroll(self):
-        return self._scroll
-
-    @property
-    def category(self):
-        return self._category
-
-    @property
-    def stats_dict(self):
-        return self.stats_dict
-
-    @property
-    def potential(self):
-        return self._potential
-
-    @property
-    def additional(self):
-        return self._additional
-
-    @property
-    def starforce_max(self):
-        return self._starforce_max
-
-    @property
-    def starforce_now(self):
-        return self._starforce_now
-
-    @property
-    def superior(self):
-        return self._superior
-
-    @property
-    def hammer(self):
-        return self._hammer
-
-
-if __name__ == "__main__":
-    my_url = "https://maplestory.nexon.com/Common/Character/Detail/%ed%9e%88%ec%8a%88%ec%99%80/Equipment?p=mikO8qgdC4hElCwBGQ6GOx8CmO11EvduZkV0bRbPAhaRzW5LQRf8%2f4r4oCWC0wKML5gmImzNTiIxKN9nZv55Kkg56a2qImGHSENhY4orAkJSLirlbSciSAERq3rRq3Ftoo99qXg0H4wl0mgR42y%2fHGGoF6ETGipGOuQfOUaPK%2f0%3d"
-
-    hishuwa = EquipInfoTag(my_url)
-    item_soup_ring = hishuwa.get_equip_info_tag(3)
-    hishuwa.browser_quit()
-    lightly_parsed = FirstParsingInfoTag(item_soup_ring)
-    print("TITLE")
-    print(lightly_parsed.title)
-    print()
-    print("CATEGORY")
-    print(lightly_parsed.category)
-    print()
-    print("STAT_DICT")
-    print(lightly_parsed.stats_dict)
-
-    heavy_parsed = SecondParsingInfoTag(lightly_parsed)
-    print(heavy_parsed._set_title())
-    print(heavy_parsed._set_scroll())
-    print(heavy_parsed._set_category())
-    print(heavy_parsed._set_stats_dict())
-    print(heavy_parsed._set_potential())
-    print(heavy_parsed._set_additional_potential())
-    print(heavy_parsed._etc_to_list())
-    print(heavy_parsed._set_starforce())
-    print(heavy_parsed._set_hammer())
-
-    # import bs4
-    # help(bs4.element.Tag)
-    # help(bs4.element.ResultSet)
-    # help(bs4.element.Tag.select)
-    # help(bs4.Tag)
